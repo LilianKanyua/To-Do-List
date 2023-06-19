befb49c2b5c76cf7ca8a743dad7e2f8986fed898
your_username <kanyualilianm.com>
2023-06-19 17:17:03 +0300

    commit

--- index.js
+++ index.js
@@ -36,20 +36,19 @@ result.id? success.innerHTML='New task successfully added': success.innerHTML= '
 
 let taskTable = document.getElementById("task-table");
 
-// Delete a task when the "Delete" button is clicked
 taskTable.addEventListener("click", function(event) {
-  // Check if the clicked element is a "Delete" button
+
   if (event.target.type === "delete") {
-    // Get the ID of the task to delete
+  
     var todoId = event.target.parentNode.id;
 
-    // Send a DELETE request to remove the task from the server
+    
     fetch(`https://dummyjson.com/todos/${todoId}`, {
       method: "DELETE"
     })
       .then(response => {
         if (response.ok) {
-          // Remove the task from the task table
+         
           let todoItem = document.getElementById(todoId);
           todoItem.remove();
           console.log("Task deleted successfully.");
@@ -60,28 +59,14 @@ taskTable.addEventListener("click", function(event) {
       .catch(error => console.error(error));
   }
 });
-// const addTask = (description) => {
-//     // Create a new JSON object.
-//     const body = {
-//       title,
-//       description,
-//       dueDate
-//     };
-  
-//     // Send the request.
-//     return fetch("https://dummyjson.com/todos/add", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Accept": "application/json"
-//       },
-//       body: JSON.stringify(body)
-//     });
+
+// document.getElementById("task-form").addEventListener("submit", async function(event) {
+//   event.preventDefault();
+
+
+//   let addedTask = document.getElementById('new-task').value;
+
+
+//   let data = {
+//     addedTask: addedTask
 //   };
-  
-//   // Add a new task.
-//   const task = await addTask("Task title", "Task description", "2023-06-15");
-  
-//   // Print the task details.
-//   console.log(task);
-  
